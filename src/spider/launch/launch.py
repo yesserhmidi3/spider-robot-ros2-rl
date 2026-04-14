d61e5301af14b5e3cc9d515544232d5f7b38fdc0
Yesser Hmidi <yesser.hmidi3@gmail.com>
2026-04-14 11:37:59 +0100

    Initial commit: Spider robot URDF and Gazebo ROS 2 Control setup

---
+++ src/spider/launch/launch.py
@@ -0,0 +1,99 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = 'spider'
+    urdf_file = 'Spider_URDF.urdf'
+
+    pkg_path = get_package_share_directory(package_name)
+    urdf_path = os.path.join(pkg_path, 'urdf', urdf_file)
+    config_path = os.path.join(pkg_path, 'config', 'controllers.yaml') # added config path to controllers.yaml to define the joints
+
+
+    # CRITICAL: This allows Gazebo to find your meshes without absolute paths
+    os.environ["GZ_SIM_RESOURCE_PATH"] = os.path.join(pkg_path, "..")
+
+    with open(urdf_path, 'r') as infp:
+        robot_desc = infp.read()
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_desc, 'use_sim_time': True}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items(),
+    )
+
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'spider_robot',
+            '-topic', 'robot_description',
+            '-x', '0', '-y', '0', '-z', '0.2', # Spawning slightly lower so it doesn't bounce
+            # STANDING POSE ARGUMENTS
+            '-J', 'L1_J2', '0.4', '-J', 'L1_J3', '0.6',
+            '-J', 'L2_J2', '0.4', '-J', 'L2_J3', '0.6',
+            '-J', 'L3_J2', '0.4', '-J', 'L3_J3', '0.6',
+            '-J', 'L4_J2', '0.4', '-J', 'L4_J3', '0.6',
+        ],
+        output='screen'
+    )
+
+    # Controller manager node // new after .yaml added
+    controller_manager = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[config_path],
+        output='screen',
+    )
+
+    # Spawn joint state broadcaster (broadcasts joint states from Gazebo)
+    spawn_jsb = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+        output='screen',
+    )
+
+    # Spawn joint trajectory controller
+    spawn_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_trajectory_controller'],
+        output='screen',
+    )
+
+    # GUI for joint control
+    '''joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )'''
+
+    # RQT GUI for joint control (alternative to joint_state_publisher_gui)
+    rqt_joint_trajectory_controller = Node(
+        package='rqt_joint_trajectory_controller',
+        executable='rqt_joint_trajectory_controller',
+        name='rqt_joint_trajectory_controller',
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        gazebo,
+        spawn_robot,
+        controller_manager,
+        spawn_jsb,  # Broadcast joint states
+        spawn_controller,  # Joint trajectory controller
+        #joint_state_publisher_gui  # GUI with sliders
+        rqt_joint_trajectory_controller # RQT GUI for joint control
+    ])
---
+++ src/spider/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'spider'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Include URDF files
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        # Include Mesh files
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        # Include config files
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yesser',
+    maintainer_email='yesser.hmidi3@gmail.com',
+    description='Spider robot description and simulation',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
